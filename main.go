10e97b875a921d6a4142d38d67c8677d2e609021
richman0116 <richman0923.rm.rm@gmail.com>
2024-07-17 05:42:36 -0700

    apply cors on handler function only

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	fmt.Fprintf(w, "Hello, World!")
 }
 
