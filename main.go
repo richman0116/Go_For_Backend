fc2767febc70c3daa3bef6bc514f499c4cb13f40
richman0116 <richman0923.rm.rm@gmail.com>
2024-07-17 05:38:34 -0700

    apply cors on only api

--- main.go
+++ main.go
@@ -5,27 +5,11 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", WithCORS(Handler))
-	http.ListenAndServe(":8080", nil)
-}
-
-// Middleware to add CORS headers to all responses
-func WithCORS(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	}
-}
-
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":8080", nil)
 }
